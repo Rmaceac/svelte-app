36aebbaf4de5ade6f6f3a98ada521ca782d7ba9c
Rmaceac <ryan.maceachern.tech@gmail.com>
2023-07-30 21:12:23 -0700

    add checkboxes

--- src/components/AddPersonForm.svelte
+++ src/components/AddPersonForm.svelte
@@ -2,12 +2,24 @@
   let name;
   let job;
   let age;
+  let fighting = false;
+  let climbing = false;
+  let swimming = false;
+
+  const handleSubmit = () => {
+    console.log(name, job, age);
+  }
 </script>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
   <input type="text" placeholder="name" bind:value={name}>
   <input type="text" placeholder="job" bind:value={job}>
   <input type="number" placeholder="age" bind:value={age}>
+  <label>Skills:<br>
+    <input type="checkbox" bind:checked={fighting}> Fighting<br>
+    <input type="checkbox" bind:checked={climbing}> Climbing<br>
+    <input type="checkbox" bind:checked={swimming}> Swimming<br>
+  </label>
   <button>Add person</button>
 </form>
 
